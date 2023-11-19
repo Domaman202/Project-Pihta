be244dad340a53f01a9f7a6c34f668574a6c69da
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2023-11-19 12:28:45 +0700

    NUPNodeAlias :)

--- src/main/kotlin/ru/DmN/phtx/pxx/PXX.kt
+++ src/main/kotlin/ru/DmN/phtx/pxx/PXX.kt
@@ -1,29 +1,29 @@
 package ru.DmN.phtx.pxx
 
+import ru.DmN.pht.std.ups.NUPNodeAlias
 import ru.DmN.pht.std.utils.StdModule
-import ru.DmN.phtx.pxx.ups.NUPAlias
 
 object PXX : StdModule("phtx/pxx") {
     init {
         // б
-        "боле" to "great"
+        "боле"           to "great"
         "боле-иль-равно" to "great-or-eq"
         // в
         "величить" to "inc"
-        "воздать" to "ret"
+        "воздать"  to "ret"
         // д
         "династия" to "cls"
         // к
-        "коли" to "if"
+        "коли"     to "if"
         // м
         "меньшить" to "dec"
-        "молвить" to "println"
+        "молвить"  to "println"
         // н
-        "не-боле" to "less"
-        "ничто" to "nil"
+        "не-боле"  to "less"
+        "ничто"    to "nil"
         // п
-        "познать" to "def"
-        "покуда" to "cycle"
+        "познать"  to "def"
+        "покуда"   to "cycle"
         // с
         "становить" to "set"
         // ц
@@ -31,6 +31,6 @@ object PXX : StdModule("phtx/pxx") {
     }
 
     infix fun String.to(alias: String) {
-        add(this, NUPAlias(alias))
+        add(this, NUPNodeAlias(alias))
     }
 }
