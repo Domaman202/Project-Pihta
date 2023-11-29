e1db67f645fea9413d2173dfe26f786cb44391cc
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2023-11-29 07:16:53 +0700

    Улучшаем Пихту в честь др сестрёнки)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 group = "ru.DmN.pht"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(files("Project-Siberia-1.0.0.jar"))
+    implementation(files("Project-Siberia-1.0.1.jar"))
     implementation(kotlin("reflect"))
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-tree:9.5")
