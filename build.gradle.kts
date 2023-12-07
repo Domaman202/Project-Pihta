9be3f8795d2916db5cecc049e1b042a536acdbb0
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2023-12-07 21:10:26 +0700

    Console update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 group = "ru.DmN.pht"
-version = "1.1.0"
+version = "1.2.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(files("Project-Siberia-1.5.1.jar", "Lazurite-2.7.0.jar"))
+    implementation(files("Project-Siberia-1.5.3.jar", "Lazurite-2.7.0.jar"))
     implementation(kotlin("reflect"))
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-tree:9.5")
