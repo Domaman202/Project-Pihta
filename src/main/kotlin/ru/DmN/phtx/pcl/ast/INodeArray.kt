a2b84726da18a138117909cf89b71de41de9f61b
DomamaN202 <vip.domaman@mail.ru>
2023-12-10 12:07:33 +0700

    PCL move

--- src/main/kotlin/ru/DmN/phtx/pcl/ast/INodeArray.kt
+++
@@ -1,9 +0,0 @@
-package ru.DmN.phtx.pcl.ast
-
-interface INodeArray {
-    val size: Int
-
-    operator fun get(index: Int): NodeElement
-
-    fun forEach(consumer: (it: NodeElement) -> Unit)
-}
--- src/main/kotlin/ru/DmN/phtx/pcl/ast/NodeLazyElement.kt
+++
@@ -1,6 +0,0 @@
-package ru.DmN.phtx.pcl.ast
-
-import ru.DmN.phtx.pcl.laxer.Laxer
-import ru.DmN.siberia.lexer.Token
-
-open class NodeLazyElement(token: Token, offset: Int, val laxer: Laxer) : NodeElement(token, offset)
