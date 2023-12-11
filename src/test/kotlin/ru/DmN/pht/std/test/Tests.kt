db5897c01e60ffb4e189e25c3b21c8b07140a98c
DomamaN202 <vip.domaman@mail.ru>
2023-12-11 16:09:43 +0700

    Tests!

--- src/test/kotlin/ru/DmN/pht/std/test/Tests.kt
+++ src/test/kotlin/ru/DmN/pht/std/test/Tests.kt
@@ -59,6 +59,16 @@ class Tests {
     }
 
     @Test
+    fun testCatch() {
+        Module("test/pht/std/catch").run {
+            compile()
+            assertEquals(test(0), 21)
+            assertEquals(test(1), 12)
+            assertFails { test(2) }
+        }
+    }
+
+    @Test
     fun testClass() {
         Module("test/pht/std/class").run {
             compile()
@@ -76,6 +86,14 @@ class Tests {
             compile()
             assertTrue((test(0) as IntArray).contentEquals(intArrayOf(12, 21, 33)))
             assertTrue((test(1) as LongArray).contentEquals(longArrayOf(202L, 203L, 213L)))
+        }
+    }
+
+    @Test
+    fun testCond() {
+        Module("test/pht/std/cond").run {
+            compile()
+            assertEquals(test(0), "МРБ")
         }
     }
 
