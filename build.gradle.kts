821c4fd963c5c6000a50fba5aaeb526f15209c18
DomamaN202 <vip.domaman@mail.ru>
2023-12-13 07:20:39 +0700

    Gradle fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,16 +27,18 @@ java {
 }
 
 tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.Console")) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+        from(contents)
+    }
+
     build {
-        dependsOn(register<Jar>("fatJar") {
-            dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
-            archiveClassifier.set("standalone")
-            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-            manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.Console")) }
-            val sourcesMain = sourceSets.main.get()
-            val contents = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
-            from(contents)
-        })
+        dependsOn(fatJar)
     }
 
     java {
