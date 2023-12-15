a2d1126f08c0995840d70ba956de1ba5c344a949
DomamaN202 <vip.domaman@mail.ru>
2023-12-15 15:55:48 +0700

    Version Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.DmN.pht"
-version = "1.6.0"
+version = "1.6.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/ru/DmN/pht/std/Console.kt
+++ src/main/kotlin/ru/DmN/pht/std/Console.kt
@@ -12,7 +12,7 @@ object Console : Console() {
         this.actions.add(Triple("О программе", "Выводит информацию о программе.", Runnable {
             println("""
                 Проект: Пихта
-                Версия: 1.4.1
+                Версия: 1.7.1
                 Авторы: DomamaN202, Wannebetheshy
             """.trimIndent())
         }))
