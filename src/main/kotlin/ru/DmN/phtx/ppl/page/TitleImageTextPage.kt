d429e4e6858875c496994d9c9cb7f6f963e2d4c5
DomamaN202 <vip.domaman@mail.ru>
2024-01-13 18:59:36 +0700

    =)

--- src/main/kotlin/ru/DmN/phtx/ppl/page/TitleImageTextPage.kt
+++ src/main/kotlin/ru/DmN/phtx/ppl/page/TitleImageTextPage.kt
@@ -9,7 +9,7 @@ import java.awt.RenderingHints
 import java.awt.geom.RoundRectangle2D
 import javax.swing.JComponent
 
-class TitleImageTextPage(val title: String, val image: Image, val text: String, val titleFont: Int, val textFont: Int) : Page() {
+class TitleImageTextPage(val title: String, val image: Image, val text: String, val titleFont: Int, val textFont: Int, val imageUp: Boolean) : Page() {
     override val component: JComponent = object : JComponent() {
         override fun paintComponent(g: Graphics?) {
             g as Graphics2D
@@ -22,19 +22,33 @@ class TitleImageTextPage(val title: String, val image: Image, val text: String, 
             g.font = Font("TimesRoman", ITALIC, textFont)
             val textMetrics = g.getFontMetrics(g.font)
             val lines = text.split('\n')
-            val textX = lines.map { (width - textMetrics.stringWidth(it)) / 2 }.average().toInt()
             val textHeight = textMetrics.height
-            var textY = height - textHeight * (lines.size + 1)
-            lines.forEach {
-                textY += textHeight
-                g.drawString(it, textX, textY)
+            if (imageUp) {
+                var textY = height - textHeight * (lines.size + 1)
+                lines.forEach {
+                    textY += textHeight
+                    g.drawString(it, textFont, textY)
+                }
+                val offset = titleY * 1.5
+                val imgWidth = width - offset * 2
+                val imgHeight = height - titleY * 2.5 - textHeight * lines.size
+                g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+                g.setClip(RoundRectangle2D.Double(offset, offset, imgWidth, imgHeight, 50.0, 50.0))
+                g.drawImage(image, offset.toInt(), offset.toInt(), imgWidth.toInt(), imgHeight.toInt(), null)
+            } else {
+                var textY = height - textHeight * (lines.size + 1)
+                lines.reversed().forEach {
+                    textY += textHeight
+                    g.drawString(it, textFont, height - textY + titleY)
+                }
+                val imgX = titleY * 1.5
+                val imgY = textHeight * (lines.size + 2)
+                val imgWidth = width - imgX * 2
+                val imgHeight = height - imgY - textHeight
+                g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+                g.setClip(RoundRectangle2D.Double(imgX, imgY.toDouble(), imgWidth, imgHeight.toDouble(), 50.0, 50.0))
+                g.drawImage(image, imgX.toInt(), imgY, imgWidth.toInt(), imgHeight, null)
             }
-            val offset = (titleY * 1.5).toInt()
-            val imgWidth = width - offset * 2
-            val imgHeight = (height - titleY * 2.5) - textHeight * lines.size
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-            g.setClip(RoundRectangle2D.Double(offset.toDouble(), offset.toDouble(), imgWidth.toDouble(), imgHeight, 50.0, 50.0))
-            g.drawImage(image, offset, offset, imgWidth, imgHeight.toInt(), null)
         }
     }
 }
