52620cc40c8e5255af9a9c3f289ef2cc352299c2
DomamaN202 <vip.domaman@mail.ru>
2024-01-14 12:27:25 +0700

    =)

--- src/main/kotlin/ru/DmN/phtx/ppl/page/Page.kt
+++ src/main/kotlin/ru/DmN/phtx/ppl/page/Page.kt
@@ -1,5 +1,6 @@
 package ru.DmN.phtx.ppl.page
 
+import ru.DmN.phtx.ppl.Presentation
 import java.awt.AlphaComposite
 import java.awt.Dimension
 import java.awt.Graphics
@@ -8,10 +9,18 @@ import javax.swing.JComponent
 
 abstract class Page {
     var blackout: Float = 0f
-    val component = object : JComponent() {
+    open val component = object : JComponent() {
         override fun paint(g: Graphics) {
             this@Page.paint(size, g as Graphics2D)
         }
+    }
+
+    open fun onShow(presentation: Presentation) {
+        presentation.frame.add(component)
+    }
+
+    open fun onHide(presentation: Presentation) {
+        presentation.frame.remove(component)
     }
 
     open fun paint(size: Dimension, g: Graphics2D) {
