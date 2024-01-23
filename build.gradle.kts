ef8b48494c4a8aba678dd83a010e0797091e61e7
DomamaN202 <vip.domaman@mail.ru>
2024-01-23 08:15:37 +0700

    Обновление часть 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.DmN.pht"
-version = "1.10.3"
+version = "1.10.4"
 
 repositories {
     mavenCentral()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("ru.DmN.siberia:Project-Siberia:1.10.1")
+    implementation("ru.DmN.siberia:Project-Siberia:1.10.2")
     implementation(kotlin("reflect"))
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-tree:9.5")
