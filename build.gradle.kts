32d910c3e172122776a3823163265496e0610438
DomamaN202 <vip.domaman@mail.ru>
2024-02-04 07:27:59 +0700

    Print update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.DmN.pht"
-version = "1.12.10"
+version = "1.12.11"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/ru/DmN/pht/ast/NodeTypedGet.kt
+++ src/main/kotlin/ru/DmN/pht/ast/NodeTypedGet.kt
@@ -3,5 +3,13 @@ package ru.DmN.pht.ast
 import ru.DmN.siberia.ast.Node
 import ru.DmN.siberia.node.INodeInfo
 import ru.DmN.siberia.utils.VirtualType
+import ru.DmN.siberia.utils.indent
 
-class NodeTypedGet(info: INodeInfo, val name: String, val type: VirtualType) : Node(info)
+class NodeTypedGet(info: INodeInfo, val name: String, val type: VirtualType) : Node(info) {
+    override fun print(builder: StringBuilder, indent: Int, short: Boolean): StringBuilder = builder.apply {
+        indent(indent).append('[').append(info.type).append('\n')
+            .indent(indent + 1).append("(name = ").append(name).append(")\n")
+            .indent(indent + 1).append("(type = ").append(type.name).append(")\n")
+            .indent(indent).append(']')
+    }
+}
