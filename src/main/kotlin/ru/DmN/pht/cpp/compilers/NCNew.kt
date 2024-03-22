457c4cc1e046a0c9152312092fb28f15901b1d86
DomamaN202 <vip.domaman@mail.ru>
2024-03-22 15:52:50 +0700

    =)

--- src/main/kotlin/ru/DmN/pht/cpp/compilers/NCNew.kt
+++ src/main/kotlin/ru/DmN/pht/cpp/compilers/NCNew.kt
@@ -2,9 +2,8 @@ package ru.DmN.pht.cpp.compilers
 
 import ru.DmN.pht.ast.NodeNew
 import ru.DmN.pht.cpp.compiler.utils.load
-import ru.DmN.pht.cpp.utils.vtype.VVTAutoPointer
+import ru.DmN.pht.cpp.utils.vtype.isAutoPointer
 import ru.DmN.pht.cpp.utils.vtype.normalizedName
-import ru.DmN.pht.utils.vtype.PhtVirtualType
 import ru.DmN.siberia.ast.INodesList
 import ru.DmN.siberia.compiler.Compiler
 import ru.DmN.siberia.compiler.ctx.CompilationContext
@@ -12,9 +11,13 @@ import ru.DmN.siberia.utils.Variable
 
 object NCNew : ICppCompiler<NodeNew> {
     override fun StringBuilder.compileVal(node: NodeNew, compiler: Compiler, ctx: CompilationContext): Variable {
-        append("gc.alloc_ptr<").append(node.type.componentType!!.normalizedName()).append('>')
-        compileArgs(node, compiler, ctx)
-        return Variable.tmp(node, VVTAutoPointer(node.type as PhtVirtualType))
+        node.type.run {
+            if (isAutoPointer)
+                append("gc.alloc_ptr<").append(componentType!!.normalizedName()).append('>')
+            else append(normalizedName())
+            compileArgs(node, compiler, ctx)
+            return Variable.tmp(node, this)
+        }
     }
 
     private fun StringBuilder.compileArgs(node: INodesList, compiler: Compiler, ctx: CompilationContext) {
